ebd5e6aa1940242c8d01f4ccdfe3424a8c64eda8
Midhun MG <midhunmgopal@gmail.com>
2019-04-27 00:00:32 +0530

    Initial commit and added gitignore file.

---
+++ AsyncDataDownloader/Library/Downloader.swift
@@ -0,0 +1,13 @@
+//
+//  Downloader.swift
+//  AsyncDataDownloader
+//
+//  Created by Midhun on 26/04/19.
+//  Copyright © 2019 Midhun. All rights reserved.
+//
+
+import UIKit
+
+class Downloader: NSObject {
+    
+}
